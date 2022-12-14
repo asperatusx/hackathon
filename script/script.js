47eaa3b0224b549b3e3e74cc2309da40d29a5745
Alec Adolph <alecdadolph@gmail.com>
2022-12-14 11:30:28 -0700

    getData function added

--- script/script.js
+++ script/script.js
@@ -2,7 +2,16 @@ axios
   .get("https://api.openbrewerydb.org/breweries")
   .then((result) => {
     console.log(result);
+    getData(result);
   })
   .catch((error) => {
     console.log(error);
   });
+
+const getData = (result) => {
+  const name = result.data[i].name;
+  const city = result.data[i].city;
+  const state = result.data[i].state;
+};
+
+f;
